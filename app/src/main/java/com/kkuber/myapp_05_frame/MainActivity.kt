24df76508c45933c0465dc074d09cac269f0b0b2
jjyu <79048655+jjyu9312@users.noreply.github.com>
2022-05-02 00:33:11 +0900

    22/05/02 \n first commit

--- app/src/main/java/com/kkuber/myapp_05_frame/MainActivity.kt
+++ app/src/main/java/com/kkuber/myapp_05_frame/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.kkuber.myapp_05_frame
 
+import android.content.Intent
+import android.content.ServiceConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.util.concurrent.Executor
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun bindIsolatedService(
+        service: Intent,
+        flags: Int,
+        instanceName: String,
+        executor: Executor,
+        conn: ServiceConnection
+    ): Boolean {
+        return super.bindIsolatedService(service, flags, instanceName, executor, conn)
     }
 }
