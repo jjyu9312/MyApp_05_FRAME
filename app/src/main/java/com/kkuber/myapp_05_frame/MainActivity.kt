1ef96b1f9b8b8aeecaf9c02291940a7475703c7f
jjyu <79048655+jjyu9312@users.noreply.github.com>
2022-05-05 22:18:13 +0900

    22/05/05 \n 사진첩 이용 가능하도록 권한 허용 팝업 만들기

--- app/src/main/java/com/kkuber/myapp_05_frame/MainActivity.kt
+++ app/src/main/java/com/kkuber/myapp_05_frame/MainActivity.kt
@@ -1,24 +1,65 @@
 package com.kkuber.myapp_05_frame
 
-import android.content.Intent
-import android.content.ServiceConnection
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import java.util.concurrent.Executor
+import android.widget.Button
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AlertDialog
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private val addPhotoBurtton : Button by lazy {
+        findViewById<Button>(R.id.addPhotoButton)
+    }
+
+    private val startPhotoFrameStartButton : Button by lazy {
+        findViewById<Button>(R.id.startPhotoFrameModeButton)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initAddPhotoButton()
+        initStartPhotoaFrameModeButton()
     }
 
-    override fun bindIsolatedService(
-        service: Intent,
-        flags: Int,
-        instanceName: String,
-        executor: Executor,
-        conn: ServiceConnection
-    ): Boolean {
-        return super.bindIsolatedService(service, flags, instanceName, executor, conn)
+    private fun initStartPhotoaFrameModeButton() {
+
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    private fun initAddPhotoButton() {
+        addPhotoBurtton.setOnClickListener {
+            when {
+                ContextCompat.checkSelfPermission(
+                    this, android.Manifest.permission.READ_EXTERNAL_STORAGE
+                ) == PackageManager.PERMISSION_GRANTED -> {
+                    // todo 권한이 잘 부여되었을 때 갤러리에서 사진 선택하는 기능 추가
+                }
+                shouldShowRequestPermissionRationale(android.Manifest.permission.READ_EXTERNAL_STORAGE) -> { // 권한이 없을 때 교육용 팝업 띄우기 위한 함수
+                    // todo 교육용 팝업 확인 후 권한 팝업을 띄우는 기능
+                    showPermissionContextPopup()
+                } else -> {
+                    requestPermissions(arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), 1000)
+                }
+            }
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    private fun showPermissionContextPopup() {
+        AlertDialog.Builder(this)
+            .setTitle("권한이 필요합니다.")
+            .setMessage("전자액자 앱에서 사진을 불러오기 위해 권한이 필요합니다.")
+            .setPositiveButton("동의하기") { _, _ ->
+                requestPermissions(arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), 1000)
+            }
+            .setNegativeButton("취소하기") { _, _ -> }
+            .create()
+            .show()
     }
 }
