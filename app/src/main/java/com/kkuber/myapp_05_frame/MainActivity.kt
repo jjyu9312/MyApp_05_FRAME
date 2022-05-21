22002335d723e50aee0be48d5e638f7064bd1c33
jjyu <79048655+jjyu9312@users.noreply.github.com>
2022-05-22 01:44:28 +0900

    22/05/22 \n SAF 기능 이용하여 image 파일만 가져오기

--- app/src/main/java/com/kkuber/myapp_05_frame/MainActivity.kt
+++ app/src/main/java/com/kkuber/myapp_05_frame/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.kkuber.myapp_05_frame
 
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AlertDialog
 import androidx.core.content.ContextCompat
@@ -19,6 +22,18 @@ class MainActivity : AppCompatActivity() {
         findViewById<Button>(R.id.startPhotoFrameModeButton)
     }
 
+    private val imageViewList: List<ImageView> by lazy {
+        mutableListOf<ImageView>().apply {
+            add(findViewById(R.id.imageView11))
+            add(findViewById(R.id.imageView12))
+            add(findViewById(R.id.imageView13))
+            add(findViewById(R.id.imageView21))
+            add(findViewById(R.id.imageView22))
+            add(findViewById(R.id.imageView23))
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -39,6 +54,7 @@ class MainActivity : AppCompatActivity() {
                     this, android.Manifest.permission.READ_EXTERNAL_STORAGE
                 ) == PackageManager.PERMISSION_GRANTED -> {
                     // todo 권한이 잘 부여되었을 때 갤러리에서 사진 선택하는 기능 추가
+                    navigatePhotos()
                 }
                 shouldShowRequestPermissionRationale(android.Manifest.permission.READ_EXTERNAL_STORAGE) -> { // 권한이 없을 때 교육용 팝업 띄우기 위한 함수
                     // todo 교육용 팝업 확인 후 권한 팝업을 띄우는 기능
@@ -48,6 +64,34 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        // 권한 요청 코드에 따른 Permission 권한 부여
+        when (requestCode) {
+            1000 -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // todo 권한이 부여됨
+                    navigatePhotos()
+                } else {
+                    Toast.makeText(this, "권한을 거부하였습니다.", Toast.LENGTH_SHORT).show()
+                }
+            }
+            else -> {
+
+            }
+        }
+    }
+
+    private fun navigatePhotos() {
+        val intent = Intent(Intent.ACTION_GET_CONTENT) // SAF 기능을 실행시켜 컨텐츠를 가져오는 안드로이드 내장 액티비티 실행시킴
+        intent.type = "image/*"
+        startActivityForResult(intent, 2000)
     }
 
     @RequiresApi(Build.VERSION_CODES.M)
